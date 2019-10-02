49790431d881dc6a7786da4e217958e9df308533
yamamotonaoki <pendonking4869@gmail.com>
2019-10-02 05:22:28 +0000

    [Add]deviseでuser・adminモデルとコントローラー追加

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,:authentication_keys => [:tell]
+
+   # usernameを必須・一意とする
+   validates_uniqueness_of :name, :team_name, :captain_name, :tell
+   validates_presence_of :name, :team_name, :captain_name, :tell
+
+  # No use email
+ def email_required?
+   false
+ end
+
+ def email_changed?
+  false
+ end
+
+ def will_save_change_to_email?
+   false
+ end
+end
